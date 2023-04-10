805ba1ebd7b793e1368f04b80b1ec22c9bda96d9
bvthanh <bvthanh1997@gmail.com>
2023-04-10 14:22:41 +0700

    first commit

---
+++ src/main/java/com/example/demobuildspring/DemoBuildSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.demobuildspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoBuildSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoBuildSpringApplication.class, args);
+	}
+
+}
