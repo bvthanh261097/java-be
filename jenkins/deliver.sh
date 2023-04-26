2707ed7d703dd3eda19f2864383b22da9fb30a87
bvthanh261097 <thuyenkhongben2610@gmail.com>
2023-04-26 15:33:19 +0700

    uo

--- jenkins/deliver.sh
+++ jenkins/deliver.sh
@@ -32,5 +32,13 @@ set +x
 
 echo 'The following command runs and outputs the execution of your Java'
 echo 'application (which Jenkins built using Maven) to the Jenkins UI.'
-set -x
-java -jar target/${NAME}-${VERSION}.jar
+echo '-------- remove file backup -------'
+sudo rm -rf /home/it_admin/java-backup/*
+echo '-------- start copy file to backup--------'
+#sudo cp /home/it_admin/java-new/hihi-0.0.1-SNAPSHOT.jar /home/it_admin/java-backup/
+echo '------start copy jar to folder java application -----'
+sudo cp target/${NAME}-${VERSION}.jar /home/it_admin/java-new/
+echo '---------copy file successfully--------------'
+sudo java -jar /home/it_admin/java-new/${NAME}-${VERSION}.jar
+#set -x
+#java -jar target/${NAME}-${VERSION}.jar
