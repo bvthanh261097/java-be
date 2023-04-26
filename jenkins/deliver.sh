83d0df5c96f400c099e32a7f136c4493592020c8
bvthanh261097 <thuyenkhongben2610@gmail.com>
2023-04-26 14:39:32 +0700

    lk

--- jenkins/deliver.sh
+++ jenkins/deliver.sh
@@ -2,7 +2,7 @@
 echo '-------- remove file backup -------'
 sudo rm -rf /home/it_admin/java-backup/*
 echo '-------- start copy file to backup--------'
-sudo cp /home/it_admin/java-new/hihi-0.0.1-SNAPSHOT.jar /home/it_admin/java-backup/
+#sudo cp /home/it_admin/java-new/hihi-0.0.1-SNAPSHOT.jar /home/it_admin/java-backup/
 echo '------start copy jar to folder java application -----'
 sudo cp /var/lib/jenkins/workspace/build test local/target/hihi-0.0.1-SNAPSHOT.jar /home/it_admin/java-new/
 echo '---------copy file successfully--------------'
